3cff19bda269c13f94bb5aebdd6eec95d8c0966f
Vladimir Romanov <vladromweb@gmail.com>
2015-11-14 19:44:58 -0500

    Configured Bootstrap

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 module OwlGrooveApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,6 +22,6 @@ module OwlGrooveApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
+
   end
 end
